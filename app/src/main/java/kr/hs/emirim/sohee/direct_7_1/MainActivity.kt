d04d145b48b4cf6b4f1c23957de8ea14e88cbaff
ksh25 <ksh251024@naver.com>
2023-08-24 20:06:11 +0900

    직접풀어보기 7-1

---
+++ app/src/main/java/kr/hs/emirim/sohee/direct_7_1/MainActivity.kt
@@ -0,0 +1,13 @@
+package kr.hs.emirim.sohee.direct_7_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title="직접 풀어보기 7-1"
+    }
+
+}
